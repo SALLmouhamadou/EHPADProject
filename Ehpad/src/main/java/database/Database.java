468e958dbe8f14a78c7c428a8cf91c63f39d9866
SALLmouhamadou <salllmouha10@gmail.com>
2022-12-11 14:17:58 +0100

    creation de classe Database, ConnexionServlet, connexion.jsp et modification index.html

---
+++ Ehpad/src/main/java/database/Database.java
@@ -0,0 +1,72 @@
+package database;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author SALL Mouhamadou
+ */
+public class Database {
+     protected static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
+    protected static final String DB_NAME = "ehpad";
+    protected static final String URL = "jdbc:mysql://localhost/" + DB_NAME + "?noAccessToProcedureBodies=true";
+    protected static final String USER = "ehpad_user";
+    protected static final String PASSWORD = "ehpad_pwd";
+    
+    protected static final int DOUBLON = 1062;
+    protected static final int ROW_IS_REFERENCED = 1451;
+    protected static final int PARENT_ROW_NOT_FOUND = 1452;
+
+    public enum SortOrder {
+        ASC, DESC;
+    }
+
+    static {
+        // Chargement du pilote
+        // Ne doit avoir lieu qu'une seule fois
+        try {
+            Class.forName(DRIVER_NAME).newInstance();
+            System.out.println("*** Driver loaded.");
+        } catch (ClassNotFoundException e) {
+            System.err.println("*** ERROR: Driver " + DRIVER_NAME + " not found");
+        } catch (InstantiationException e) {
+            System.err.println("*** ERROR: Impossible to create an instance of " + DRIVER_NAME);
+            System.err.println(e.getMessage());
+        } catch (IllegalAccessException e) {
+            System.err.println("*** ERROR: Impossible to create an instance of " + DRIVER_NAME);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Fournit une connexion à la base de données. Ne fait pas appel à un pool
+     * de connexion, mâme si cela est envisageable plus tard (ne changerait rien
+     * à l'appel de la méthode)
+     *
+     * @throws java.sql.SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    /**
+     * Réinitialise la base de données en appelant la procédure stockée
+     * reset_data().
+     *
+     * @throws SQLException
+     */
+    public static void reset(LocalDateTime dateEffet) throws SQLException {
+        Connection connection = Database.getConnection();
+        CallableStatement stmt = connection.prepareCall("CALL reset_data(?)");
+        stmt.setTimestamp(1, Timestamp.valueOf(dateEffet));
+        stmt.execute();
+        stmt.close();
+        connection.close();
+    }
+}
