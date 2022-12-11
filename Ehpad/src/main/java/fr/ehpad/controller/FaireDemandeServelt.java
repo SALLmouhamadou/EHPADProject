517ea2bea4bd07ac710c10506d66c527658a0b5f
SALLmouhamadou <salllmouha10@gmail.com>
2022-12-11 15:52:14 +0100

    creation de classe Personne, FaireDemandeServlet, candidat.jsp et modification index.html

---
+++ Ehpad/src/main/java/fr/ehpad/controller/FaireDemandeServelt.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package fr.ehpad.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author SALL Mouhamadou
+ */
+@WebServlet(name = "FaireDemandeServelt", urlPatterns = {"/faireSaDemande"})
+public class FaireDemandeServelt extends HttpServlet {
+
+    private final static String VUE = "/WEB-INF/candidat.jsp";
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher(VUE).forward(request, response);
+    }
+
+  
+}
---
+++ Ehpad/src/main/webapp/WEB-INF/candidat.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : candidat
+    Created on : 11 déc. 2022, 15:37:11
+    Author     : SALL Mouhamadou
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+       Candidat
+    </body>
+</html>
