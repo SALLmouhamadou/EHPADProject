3cedaff92d061aa9c8a74fc6eb6ee4b002c03d40
SALLmouhamadou <salllmouha10@gmail.com>
2022-12-13 15:57:07 +0100

    renomme CandidatDao car souci IDE

--- Ehpad/src/main/java/fr/ehpad/dao/CandidatsDao.java
+++ Ehpad/src/main/java/fr/ehpad/dao/CandidatsDao.java
@@ -1,8 +1,7 @@
-package fr.ehpad.Dao;
-
-import database.Database;
+package fr.ehpad.dao;
 import fr.ehpad.entity.Personne;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -10,7 +9,7 @@ import java.sql.SQLException;
  *
  * @author SALL Mouhamadou
  */
-public class CandidatDao {
+public class CandidatsDao {
     public static void insert(Personne candidat) throws SQLException{
         Connection connexion = Database.getConnection();
         String  sql ="INSERT INTO personne( nom, prenom, email, pwd, telephone) VALUES(?, ?, ?, ?, ?)";
@@ -18,8 +17,9 @@ public class CandidatDao {
         stmt.setString(1, candidat.getNom());
         stmt.setString(2, candidat.getPrenom());
         stmt.setString(3, candidat.getEmail());
-        stmt.setString(4, candidat.getPassword());
+        stmt.setString(4, candidat.getPwd());
         stmt.setString(5, candidat.getTelephone());
+        stmt.setDate(8, Date.valueOf(candidat.getDateNaissance()));
         stmt.executeUpdate();
         connexion.close();
     }
