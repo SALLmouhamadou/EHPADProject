de86c054065c36ade00ea2cb5d9c3c08d06db957
Damien <seal.dofus@gmail.com>
2022-12-13 15:34:26 +0100

    Vrai premiere version de PrescriptionDAO

--- Ehpad/src/main/java/fr/ehpad/dao/PrescriptionDAO.java
+++ Ehpad/src/main/java/fr/ehpad/dao/PrescriptionDAO.java
@@ -21,40 +21,17 @@ public class PrescriptionDAO {
 		state.setInt(1, idPensionnaire);
 		ResultSet res = state.executeQuery();
 		res.next();
-		while (res.next()) {
-			Integer idMedicament = res.getInt("id_medicament");
-			String noRPPS = res.getString("no_RPPS");
-			Integer idPersonne = res.getInt("id_personne");
-			LocalDate jour = LocalDate.parse(res.getString("jour"));
-			String posologie = res.getString("posologie");
-			LocalDate dateDebutTraitement = LocalDate.parse(res.getString("date_debut_traitement"));
-			LocalDate dateFinTraitement = LocalDate.parse(res.getString("date_fin_traitement"));
-			
-			Prescription pres = new Prescription(idMedicament, idPersonne, jour, dateDebutTraitement, dateFinTraitement, posologie, noRPPS);
-			
-			String sql2 = "SELECT * FROM medicament WHERE id_medicament=?;";
-			String medicament = res.getString("id_medicament");
-			PreparedStatement state2 = con.prepareCall(sql2);
-			state2.setInt(1, idMedicament);
-			ResultSet res2 = state2.executeQuery();
-			res2.next();
-			while (res2.next()) {
-				String medicNom = res2.getString("nom");
-				String medicFonction = res2.getString("fonction");
-				String stock = res2.getString("stock");
-				System.out.println(medicNom + " " + medicFonction + " " + stock);
-			}
-			System.out.println(medicament + " ID : " + posologie);
-			return null;
-		}
+		Integer idMedicament = res.getInt("id_medicament");
+		String noRPPS = res.getString("no_RPPS");
+		Integer idPersonne = res.getInt("id_personne");
+		LocalDate jour = LocalDate.parse(res.getString("jour"));
+		String posologie = res.getString("posologie");
+		LocalDate dateDebutTraitement = LocalDate.parse(res.getString("date_debut_traitement"));
+		LocalDate dateFinTraitement = LocalDate.parse(res.getString("date_fin_traitement"));
 
-		PreparedStatement canal2 = con.prepareStatement("select * from exemple where id= ?");
-		canal2.setInt(1, 0);
-		ResultSet res2 = canal2.executeQuery();
-		while (res2.next()) {
-			String n = res2.getString("nom");
-			int i = res2.getInt("id");
-			System.out.println(n);
-		}
+		Prescription pres = new Prescription(idMedicament, idPersonne, jour, dateDebutTraitement, dateFinTraitement,
+				posologie, noRPPS);
+
+		return pres;
 	}
 }
